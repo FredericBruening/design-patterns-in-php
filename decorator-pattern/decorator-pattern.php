c0a5812a967d1dec3cc3d1192cf519f894933948
FredericBruening <contact@fredesk.com>
2021-05-06 17:05:48 -0500

    add the decorator pattern

---
+++ decorator-pattern/decorator-pattern.php
@@ -0,0 +1,67 @@
+<?php
+/* Sometimes decorating is better and inheriting
+ * following the O Principle "open for extension, closed for modification"
+ * an example of the decorator pattern
+*/
+
+interface CarService {
+    public function getCost();
+    public function getDescription();
+}
+
+class BasicInspection implements CarService {
+
+    public function getCost()
+    {
+        return 25;
+    }
+
+    public function getDescription()
+    {
+        return 'Basic inspection';
+    }
+}
+
+class OilChange implements CarService {
+
+    protected $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 29 + $this->carService->getCost();
+    }
+
+    public function getDescription()
+    {
+        return $this->carService->getDescription() . ', and oil change';
+    }
+}
+
+class TireRotation implements CarService {
+
+    protected $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 29 + $this->carService->getCost();
+    }
+
+    public function getDescription()
+    {
+        return $this->carService->getDescription() . ', and a tire rotation';
+    }
+}
+
+$service = new OilChange(new TireRotation(new BasicInspection()));
+
+echo $service->getDescription();
